31f845aec127446ab503b6ca02b28c470d24e8e7
Vinicius Alves Sousa <vinicius860@yahoo.com.br>
2026-03-18 18:02:18 -0300

    feat: implementacao do authModule

---
+++ backend/src/authUser/auth.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+
+@Module({
+    imports: [PrismaService],
+    controllers: [AuthController],
+    providers: [AuthService]
+})
+
+export class AuthModule {};
