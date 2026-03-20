65f5aca52be5ba67e7de85515d45c60abcc3d192
Vinicius Alves Sousa <vinicius860@yahoo.com.br>
2026-03-20 12:45:08 -0300

    feat: adiciona configuração inicial do JWT no auth module

--- backend/src/authUser/auth.module.ts
+++ backend/src/authUser/auth.module.ts
@@ -2,9 +2,15 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET || 'dev-secret',
+            signOptions: {expiresIn: '1d'},
+        })
+    ],
     controllers: [AuthController],
     providers: [AuthService]
 })
