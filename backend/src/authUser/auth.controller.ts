51053f5f02a422794835a9c14ca07ba7ae2291fc
Vinicius Alves Sousa <vinicius860@yahoo.com.br>
2026-03-23 14:44:42 -0300

    feat: adiciona rota protegida /auth/me com JwtAuthGuard

--- backend/src/authUser/auth.controller.ts
+++ backend/src/authUser/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { CreateUserDto } from "src/dto/dto.users/dto.user";
 import { LoginDto } from "src/dto/dto.login/dto.login";
+import { JwtAuthGuard } from "src/guards/jwt-auth.guard";
+import { GuardsConsumer } from "@nestjs/core/guards";
 
 @Controller('auth')
 export class AuthController {
@@ -15,5 +17,11 @@ export class AuthController {
     @Post('login')
     login(@Body() dto: LoginDto) {
         return this.authService.login(dto);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('me')
+    me() {
+        return "Rota protegida";
     }
 }
