22ea146d77f5bdb5264a8cf79643ba93c50321c7
Vinicius Alves Sousa <vinicius860@yahoo.com.br>
2026-03-30 22:28:40 -0300

    feat: cria os create dos DTOs do projects

---
+++ backend/src/auth/dto/dto.projects/dto.projects.create.ts
@@ -0,0 +1,34 @@
+import { IsEnum, IsNumber, IsOptional, IsString, IsUrl, MaxLength, Min, IsEthereumAddress } from 'class-validator';
+import { Type } from 'class-transformer';
+import { ProjectStatus } from 'generated/prisma/enums';
+
+export class CreateProjectDto {
+  @IsString()
+  @MaxLength(150)
+  title: string;
+
+  @IsString()
+  @MaxLength(2000)
+  description: string;
+
+  @Type(() => Number)
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @Min(0)
+  goalAmount: number;
+
+  @IsOptional()
+  @IsString()
+  ownerId?: string;
+
+  @IsOptional()
+  @IsUrl()
+  imageUrl?: string;
+
+  @IsOptional()
+  @IsEthereumAddress()
+  contractAddress?: string;
+
+  @IsOptional()
+  @IsEnum(ProjectStatus)
+  status?: ProjectStatus;
+}
