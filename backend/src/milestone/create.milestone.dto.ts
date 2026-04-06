8d0f5b5787e168cb3fae7cf1abd582f707498486
Vinicius Alves Sousa <vinicius860@yahoo.com.br>
2026-04-06 12:32:53 -0300

    feat: cria classe do DTO do milestone(create)

---
+++ backend/src/milestone/create.milestone.dto.ts
@@ -0,0 +1,22 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsNumber, IsString, MaxLength, Min } from 'class-validator';
+
+export class CreateMilestoneDto {
+  @IsString()
+  @MaxLength(150)
+  title: string;
+
+  @IsString()
+  @MaxLength(2000)
+  description: string;
+
+  @Type(() => Number)
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @Min(0.01)
+  amount: number;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  order: number;
+}
