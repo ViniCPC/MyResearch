b90912ec30cbe74a9777b0f50e4f9e571ed2bc20
Vinicius Alves Sousa <vinicius860@yahoo.com.br>
2026-04-16 17:30:48 -0300

    feat: inicializa frontend com react e vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>MyResearch</h1>
+      <p>Frontend iniciado com React + Vite</p>
+    </div>
+  );
+}
+
+export default App;
